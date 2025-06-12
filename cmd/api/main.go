e11f591eea415f4aa80ef9f024e83ec4d501391b
Maksim Evtyagin <evtyagin.maksim@gmail.com>
2025-06-12 15:03:39 +0300

    Bind Echo handlers and routes

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"github.com/labstack/echo/v4"
+	"log"
+	"maxwellzp/blog-api/internal/handler"
+	"maxwellzp/blog-api/internal/repository"
+	"maxwellzp/blog-api/internal/service"
+)
+
+import (
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Blog")
+
+	// Load environment variables
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found")
+	}
+
+	userRepo := repository.NewUserRepository(nil)
+	authService := service.NewAuthService(userRepo)
+	authHandler := handler.NewAuthHandler(authService)
+
+	e := echo.New()
+	e.POST("/register", authHandler.Register)
+	e.POST("/login", authHandler.Login)
 }
