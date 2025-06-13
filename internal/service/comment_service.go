b041e5424a64c24dc7efe773342975b90bb527df
Maksim Evtyagin <evtyagin.maksim@gmail.com>
2025-06-13 15:30:36 +0300

    Create Comment Service

---
+++ internal/service/comment_service.go
@@ -0,0 +1,68 @@
+package service
+
+import (
+	"context"
+	"errors"
+	"maxwellzp/blog-api/internal/model"
+	"maxwellzp/blog-api/internal/repository"
+	"strings"
+)
+
+type CommentService interface {
+	Create(ctx context.Context, userID, blogID int64, content string) (*model.Comment, error)
+	GetByID(ctx context.Context, id int64) (*model.Comment, error)
+	Update(ctx context.Context, id int64, content string) error
+	Delete(ctx context.Context, id int64) error
+	ListByBlogID(ctx context.Context, blogID int64) ([]*model.Comment, error)
+}
+
+type commentService struct {
+	repo repository.CommentRepository
+}
+
+func NewCommentService(repo repository.CommentRepository) CommentService {
+	return &commentService{repo: repo}
+}
+
+func (s *commentService) Create(ctx context.Context, userID, blogID int64, content string) (*model.Comment, error) {
+	content = strings.TrimSpace(content)
+	if content == "" {
+		return nil, errors.New("content is empty")
+	}
+
+	comment := &model.Comment{
+		UserID:  userID,
+		BlogID:  blogID,
+		Content: content,
+	}
+
+	if err := s.repo.Create(ctx, comment); err != nil {
+		return nil, err
+	}
+	return comment, nil
+}
+
+func (s *commentService) GetByID(ctx context.Context, id int64) (*model.Comment, error) {
+	return s.repo.GetByID(ctx, id)
+}
+
+func (s *commentService) Update(ctx context.Context, id int64, content string) error {
+	content = strings.TrimSpace(content)
+	if content == "" {
+		return errors.New("content cannot be empty")
+	}
+
+	comment := &model.Comment{
+		ID:      id,
+		Content: content,
+	}
+	return s.repo.Update(ctx, comment)
+}
+
+func (s *commentService) Delete(ctx context.Context, id int64) error {
+	return s.repo.Delete(ctx, id)
+}
+
+func (s *commentService) ListByBlogID(ctx context.Context, blogID int64) ([]*model.Comment, error) {
+	return s.repo.ListByBlogID(ctx, blogID)
+}
