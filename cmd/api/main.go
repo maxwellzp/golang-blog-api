2bf070daa118ced9aca5af1f4c71685e4f82fdc0
Maksim Evtyagin <evtyagin.maksim@gmail.com>
2025-06-13 15:57:50 +0300

    Instantiate Comment repo, Comment service and Comment handler. Bind the handlers and the router

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,15 +1,12 @@
 package main
 
 import (
+	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 	"log"
 	"maxwellzp/blog-api/internal/handler"
 	"maxwellzp/blog-api/internal/repository"
 	"maxwellzp/blog-api/internal/service"
-)
-
-import (
-	"github.com/joho/godotenv"
 )
 
 func main() {
@@ -27,6 +24,10 @@ func main() {
 	blogService := service.NewBlogService(blogRepo)
 	blogHandler := handler.NewBlogHandler(blogService)
 
+	commentRepo := repository.NewCommentRepository(nil)
+	commentService := service.NewCommentService(commentRepo)
+	commentHandler := handler.NewCommentHandler(commentService)
+
 	e := echo.New()
 	e.POST("/register", authHandler.Register)
 	e.POST("/login", authHandler.Login)
@@ -36,5 +37,10 @@ func main() {
 	e.GET("/blogs/:id", blogHandler.GetByID)
 	e.PUT("/blogs/:id", blogHandler.Update)
 	e.DELETE("/blogs/:id", blogHandler.Delete)
-	
+
+	e.POST("/comments", commentHandler.Create)
+	e.GET("/comments/:id", commentHandler.GetByID)
+	e.PUT("/comments/:id", commentHandler.Update)
+	e.DELETE("/comments/:id", commentHandler.Delete)
+	e.GET("/blogs/:blog_id/comments", commentHandler.ListByBlogID)
 }
